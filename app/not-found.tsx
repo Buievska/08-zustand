f331bd49ddb193adacb2440d0dfb0b118dd825f5
Ania <anna111239@gmail.com>
2025-10-15 19:50:32 +0200

    1/4

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,7 +15,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "100px" }}>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for doesn't exist.</p>
+      <p>{"Sorry, the page you're looking for doesn't exist."}</p>
     </div>
   );
 }
